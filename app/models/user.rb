2d421e89971f81f89800895b3c0e2ee003e92119
Neemias Freitas <neemiasvf@gmail.com>
2019-11-25 02:16:22 -0300

    Delete tweets from user when cancelling account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
 
-  has_many :tweets, inverse_of: :user
+  has_many :tweets, inverse_of: :user, dependent: :destroy
 
   validates :full_name, :email, :encrypted_password, presence: true
   validates :email, uniqueness: { case_sensitive: false }
